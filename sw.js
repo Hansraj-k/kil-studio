ecdafc3f07c1e086c739aec12eb89af022984206
Hansraj <hans182007@gmail.com>
2022-05-09 12:50:06 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,11 +1,15 @@
 self.addEventListener("install", e => {
     e.waitUntill(
         caches.open("static").then(cache => {
-            return cache.addAll(["./","style.css","p404.png"]);
+            return cache.addAll(["./","style.css","images/kil.png"]);
         })
     );
 });
 
 self.addEventListener("fetch", e => {
-    console.log('Intercepting fetch request for: ${e.request.url}');
+    e.respondWith(
+        caches.match(e.request).then(response =>{
+            return response || fetch(e.request);
+        })
+    );
 });
