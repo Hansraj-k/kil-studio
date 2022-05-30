07ee31ff12db8a4c2594053899742e05f4f840e9
Hansraj <hans182007@gmail.com>
2022-05-30 16:21:08 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,7 +1,7 @@
 self.addEventListener("install", e => {
     e.waitUntil(
         caches.open("static").then(cache => {
-            return cache.addAll(["./","./style.css","./images/kil.png"]);
+            return cache.addAll(["./style.css","./images/kil.png"]);
         })
     );
 });
